bd6148da57c3904f20b02bb584673850af4d699c
margret edeh <sagaedeh@gmail.com>
2022-08-16 15:28:19 +0100

    included all necessary functionalities

--- randomColor/secondary.js
+++ randomColor/secondary.js
@@ -1,12 +1,11 @@
-let colors= ["red", "blue", "yellow" ] //red=0, blue=1, yellow=2
+let colors= ["orange", "green", "magenta", "olive", "purple"," violet", "indigo",] 
 let btn = document.getElementById("colorBtn")
 let text = document.querySelector(".textColor")
 btn.addEventListener("click" ,function(){
 let randomColor= getRandomColor();
 document.body.style.backgroundColor = colors[randomColor];
 text.textContent = colors[randomColor]
-document.getElementById("colorBtn").style.transform= "rotateX(360deg)";//a kind of animation to the button element
- 
+document.getElementById("colorBtn").style.transform= "rotateX(360deg)";
 
 })
 function getRandomColor(){
